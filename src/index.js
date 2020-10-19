36a135772e597d97a613dc39b056a505d82feee9
alinalitvinka <alinalitvinko7@gmail.com>
2020-10-19 13:42:58 +0300

    init: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig) {  
+  let n = str.length;
+  for (i = 0; i <= n/3; i++) {
+    str = str.replace(/\{\}|\[\]|\(\)|\|\||12|34|56|77|88/g, '');
+   }
+   if (str === '') {
+     return true;
+   }
+  return false;
 }
